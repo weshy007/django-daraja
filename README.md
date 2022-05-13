e5d448751c0b19d51beac1cef9c24aa4319f0c4f
weshy <josephwaweru96@gmail.com>
2022-05-13 11:45:03 +0300

    sample .env, update README, swagger documentation

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-## Safaricom Daraja Project
+## Safaricom Daraja API Project
 - An API that consumes the daraja v2.0 endpoints and helps with Lipa na Mpesa function
+- It has one endpoint and more to come
 
 
 <!-- ![An old rock in the desert](/assets/sdk.jpeg "Generated MPESA STK Push") -->
--- safaricom/urls.py
+++ safaricom/urls.py
@@ -16,7 +16,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Daraja API",
+      default_version='v1',
+      description="An App that consumes the daraja v2.0 endpoints and helps with Lipa-na-Mpesa function",
+      terms_of_service="https://github.com/weshy007/django-daraja/blob/main/LICENCE",
+      contact=openapi.Contact(email="josephwaweru96@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('daraja.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
