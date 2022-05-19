0a72e47afb1f714ddc970c6f879af8b7abea42d4
weshy <josephwaweru96@gmail.com>
2022-05-19 15:57:11 +0300

    settings deploy

--- safaricom/settings.py
+++ safaricom/settings.py
@@ -56,7 +56,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
